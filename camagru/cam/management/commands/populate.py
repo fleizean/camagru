b868b9ec026d8e768266a8ae6a4ec842fa0c3769
Enes <nsyagz@gmail.com>
2024-07-02 00:39:26 +0300

    chore: Add script to populate database with fake data

---
+++ camagru/cam/management/commands/populate.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+from cam.models import UserProfile, Image, Comment, Like
+from faker import Faker
+import random
+import os
+
+class Command(BaseCommand):
+    help = 'Populate database with fake data'
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+        for i in range(10):
+            user = UserProfile.objects.create(
+                username=fake.user_name(),
+                email=fake.email(),
+                password=fake.password(),
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                bio=fake.text()
+            )
+            for j in range(5):
+                image = Image.objects.create(
+                    user=user,
+                    image="images/deneme/deneme_vuuxPFa.webp",
+                )
+                for k in range(3):
+                    Comment.objects.create(
+                        user=user,
+                        image=image,
+                        comment=fake.text()
+                    )
+                    Like.objects.create(
+                        user=user,
+                        image=image
+                    )
+        self.stdout.write(self.style.SUCCESS('Successfully populated database'))
