e201130dd8875d8fe0f6eaa0081c1d6cafb587df
Enes <nsyagz@gmail.com>
2024-07-06 20:43:25 +0300

    chore: Update email templates for account activation and password reset. Settings all clean for env rules.

--- camagru/camagru/settings.py
+++ camagru/camagru/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sk*%9ulyq6wiq9n$c+sba(112lmrhv9#+1ye3d49&+pbg@sz@7" # environ.get("SECRET_KEY")
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get("DEBUG", default='True') == 'True'
@@ -171,8 +171,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'github' # environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = 'github' # environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
